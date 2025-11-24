a2fabbf3a43d4143132c292813fa32257a8b8d8c
DiopBPape <pape.diop@itsrizzoli.it>
2025-11-24 11:50:21 +0100

    file back-end

---
+++ Back-end/FastLicense/src/main/java/its/rizzoli/FastLicense/FastLicenseApplication.java
@@ -0,0 +1,13 @@
+package its.rizzoli.FastLicense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastLicenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastLicenseApplication.class, args);
+	}
+
+}
---
+++ Back-end/FastLicense/src/test/java/its/rizzoli/FastLicense/FastLicenseApplicationTests.java
@@ -0,0 +1,13 @@
+package its.rizzoli.FastLicense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastLicenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
